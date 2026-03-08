24ae507321503de0eb14f463f1057655c8768604
BONIFAZ282 <69992871+BONIFAZ282@users.noreply.github.com>
2026-03-08 15:40:04 -0500

    feat: configuraciones para subir a produccion

--- microservice-personal/src/test/java/com/serviciosariana/app/personal/MicroservicePersonalApplicationTests.java
+++ microservice-personal/src/test/java/com/serviciosariana/app/personal/MicroservicePersonalApplicationTests.java
@@ -2,8 +2,10 @@ package com.serviciosariana.app.personal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MicroservicePersonalApplicationTests {
 
 	@Test
